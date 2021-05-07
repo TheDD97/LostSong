9a38050bbb63e1c7176d4dbcc8a776352a0ccd3e
Your Name <pntdnc97h28d976l@studenti.unical.it>
2021-05-07 02:01:08 +0200

    add 2 columns on tileContainer, add 2 column button on activity_game.xml, set column buttons trasparent, change tap bar

--- app/src/main/java/com/example/lostsong/Home.java
+++ app/src/main/java/com/example/lostsong/Home.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Home extends AppCompatActivity {
     private ImageButton imageButton;
      private LinearLayout layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
