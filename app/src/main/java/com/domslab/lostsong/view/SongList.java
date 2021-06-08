190a0fa572d09c02cdfa4eebc61f8d97b4d32e28
TheDD97 <pntdnc97h28d976l@studenti.unical.it>
2021-06-08 18:10:50 +0200

    add exit button on SongList

--- app/src/main/java/com/domslab/lostsong/view/SongList.java
+++ app/src/main/java/com/domslab/lostsong/view/SongList.java
@@ -1,9 +1,13 @@
 package com.domslab.lostsong.view;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Process;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -24,6 +28,7 @@ public class SongList extends AppCompatActivity implements SongAdapter.OnSongLis
     SongAdapter songAdapter;
     private DbManager db;
     private Button howToPlayButton;
+    private ImageButton closeAppButton;
 
     private DbManager getDatabaseManager() {
         if (db == null)
@@ -40,8 +45,8 @@ public class SongList extends AppCompatActivity implements SongAdapter.OnSongLis
         SharedPreferences.Editor editor = getSharedPreferences(TilesSurfaceView.settingName, MODE_PRIVATE).edit();
         editor.putInt("Time", 0);
         editor.apply();
-        Integer[] songLogo = {R.drawable.dream, R.drawable.mg, R.drawable.fairy_tail, R.drawable.me3, R.drawable.vivy,R.drawable.dragon_quest,R.drawable.black_clover,R.drawable.edens_zero,R.drawable.sao_alicization};
-        String[] songName = {"Dream", "Megalovania", "Fairy tail main theme", "An end once and for all", "Vivy fluorite eyes song","Dragon quest Opening 2020","Haruka Mirai","Edens through the rough","LiSA - ADAMAS"};
+        Integer[] songLogo = {R.drawable.dream, R.drawable.mg, R.drawable.fairy_tail, R.drawable.me3, R.drawable.vivy, R.drawable.dragon_quest, R.drawable.black_clover, R.drawable.edens_zero, R.drawable.sao_alicization};
+        String[] songName = {"Dream", "Megalovania", "Fairy tail main theme", "An end once and for all", "Vivy fluorite eyes song", "Dragon quest Opening 2020", "Haruka Mirai", "Edens through the rough", "LiSA - ADAMAS"};
         songCards = new ArrayList<>();
         for (int i = 0; i < songLogo.length; i++) {
             SongCard tmp = new SongCard(songLogo[i], songName[i]);
@@ -58,9 +63,34 @@ public class SongList extends AppCompatActivity implements SongAdapter.OnSongLis
         LinearLayoutManager layoutManager = new LinearLayoutManager(SongList.this, LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-
         songAdapter = new SongAdapter(SongList.this, songCards, this);
         recyclerView.setAdapter(songAdapter);
+        closeAppButton = findViewById(R.id.closeButton);
+        closeAppButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(SongList.this);
+                builder.setMessage("Do you want to exit?");
+                builder.setCancelable(true);
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        Process.killProcess(Process.myPid());
+                        System.exit(0);
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+            }
+        });
 
     }
 
@@ -105,4 +135,5 @@ public class SongList extends AppCompatActivity implements SongAdapter.OnSongLis
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
     }
+
 }
